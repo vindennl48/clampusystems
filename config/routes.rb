b204b969a4c7294a4042140441fad22bc711359b
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2019-05-12 16:02:42 -0400

    Updated customer edit index and show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root "customers#index"
-  resources :customers, only: [:new, :create, :update, :edit, :index]
+  resources :customers, only: [:new, :create, :update, :edit, :show, :index]
 end

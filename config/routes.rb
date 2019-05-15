7242b06b5044fad50ecb2ce5b020816b2b84ec5e
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2019-05-15 16:13:10 -0400

    Added basic navbar and changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :packages
-  resources :shipping_companies
-  root "customers#index"
-  resources :customers,   only: [:new, :create, :update, :edit, :show, :index, :destroy]
-  resources :addresses,   only: [:new, :create, :update, :edit, :show, :destroy]
-  resources :orders,      only: [:new, :create, :update, :edit, :show, :index, :destroy]
-  resources :order_items, only: [:new, :create, :update, :edit, :show, :destroy]
+  root "orders#index"
+  resources :customers,          only: [:new, :create, :update, :edit, :show, :index, :destroy]
+  resources :addresses,          only: [:new, :create, :update, :edit, :show, :destroy]
+  resources :orders,             only: [:new, :create, :update, :edit, :show, :index, :destroy]
+  resources :order_items,        only: [:new, :create, :update, :edit, :show, :destroy]
+  resources :packages,           only: [:new, :create, :update, :edit, :show, :destroy]
+  resources :shipping_companies, only: [:new, :create, :update, :edit, :show, :index, :destroy]
 end

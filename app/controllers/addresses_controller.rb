3fb8cda6aaa1a28d089bbeda4d83fa00f64c557f
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2019-05-15 12:38:33 -0400

    Fixed destroy route for addresses and other minor things

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,5 +1,6 @@
 class AddressesController < ApplicationController
   before_action :set_address, only: [:show, :edit, :update, :destroy]
+  before_action :set_customer, only: [:destroy]
 
   # GET /addresses
   # GET /addresses.json
@@ -57,7 +58,7 @@ class AddressesController < ApplicationController
   def destroy
     @address.destroy
     respond_to do |format|
-      format.html { redirect_to addresses_url, notice: 'Address was successfully destroyed.' }
+      format.html { redirect_to @customer, notice: 'Address was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -66,6 +67,11 @@ class AddressesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_address
       @address = Address.find(params[:id])
+    end
+
+    def set_customer
+      @customer = @address.customer
+      puts "Customer: #{@customer}"
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "customers#index"
-  resources :customers, only: [:new, :create, :update, :edit, :show, :index]
-  resources :addresses, only: [:new, :create, :update, :edit, :show]
+  resources :customers, only: [:new, :create, :update, :edit, :show, :index, :destroy]
+  resources :addresses, only: [:new, :create, :update, :edit, :show, :destroy]
 end

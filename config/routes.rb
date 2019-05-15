63b47b3c1f2bd71427aa840b7d2e836780247957
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2019-05-15 13:52:32 -0400

    Changed some routing for orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :orders
   root "customers#index"
   resources :customers, only: [:new, :create, :update, :edit, :show, :index, :destroy]
   resources :addresses, only: [:new, :create, :update, :edit, :show, :destroy]
+  resources :orders,    only: [:new, :create, :update, :edit, :show, :index, :destroy]
 end

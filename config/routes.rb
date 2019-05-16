1d9d870731996209193b11c93ab2ddff03cb8d0b
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2019-05-16 09:37:10 -0400

    Fixed a lot of formatting and css

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "orders#index"
+  root "customers#index"
   resources :customers,          only: [:new, :create, :update, :edit, :show, :index, :destroy]
   resources :addresses,          only: [:new, :create, :update, :edit, :show, :destroy]
   resources :orders,             only: [:new, :create, :update, :edit, :show, :index, :destroy]

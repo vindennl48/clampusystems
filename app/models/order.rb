dc91f422caf2ea397c053f159924bd9a8ddea86c
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2019-05-21 18:30:27 -0400

    Updated link names and currency displays

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,16 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items
   has_many :packages
+
+  def total
+    total = 0.0
+    self.order_items.each do |item|
+      total += item.quantity * item.unit_price
+    end
+    self.packages.each do |package|
+      total += package.charged
+    end
+    return total
+  end
+
 end

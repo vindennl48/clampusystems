707cae4085abd361411bf78fb1b7960e436f2277
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2019-05-26 13:06:23 -0400

    Updated views with credit card info
    To only show the last 4 of the credit cards so as to prevent
    users from seeing what the full credit card information is.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,4 +2,25 @@ class Payment < ApplicationRecord
   belongs_to :payment_type
   belongs_to :order
   belongs_to :credit_card, optional: true
+  before_update :set_credit_card
+
+  def get_cards
+    result = [["None", nil]]
+
+    self.order.customer.credit_cards.each do |card|
+      result.push([
+        "...#{card.number.to_s[-4..-1]}",
+        card.id,
+      ])
+    end
+
+    return result
+  end
+
+  def set_credit_card
+    if self.payment_type_id != 4
+      self.credit_card_id = nil
+    end
+  end
+
 end

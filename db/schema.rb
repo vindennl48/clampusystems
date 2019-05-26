0d2630114698535b0762d5ed643d1c404fdf7dd7
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2019-05-26 14:49:10 -0400

    Added notes to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_26_025821) do
+ActiveRecord::Schema.define(version: 2019_05_26_184554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2019_05_26_025821) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "active"
+    t.string "notes"
     t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
